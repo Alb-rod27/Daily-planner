d66164d6902a5b1a04ef2dfe8e3317e7da2da956
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-04-08 12:31:23 -0700

    fixed time colors and icons

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 //add current day to the top of the calender.
-let day = moment().format("dddd, MMMM Do YYYY");
+let day = moment().format("dddd MMMM Do YYYY, h:mm a");
 $("#currentDay").html(day);
 
 
@@ -16,29 +16,34 @@ $("#currentDay").html(day);
 //     "16":[],
 //     "17":[]
 // }
+$("saveBtn").click(function () {
+    var input = $(this).child(".description").val();
+    var time = $(this).parent().attr("id");
+    localStorage.setItem(input, time)
+});
 
 //indicate whether the the time blocks are past, present, future. If/else statment might help, use military time.
 function timer() {
 
     var currentTime = moment().hour()
 
-    $('.time-block').each(function () {
-        var currentTask = parseInt($("#text").attr('id'));
+    $(".time-block").each(function () {
+        var currentTask = parseInt($(this).attr("id"));
 
-        if ((currentTask) < currentTime) {
-            $("#text").removeClass("future");
-            $("#text").removeClass("present");
-            $("#text").addClass("past");
+        if (currentTask < currentTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("present");
+            $(this).addClass("past");
         }
-        else if ((currentTask) === currentTime) {
-            $("#text").removeClass("future");
-            $("#text").removeClass("past");
-            $("#text").addClass("present");
+        else if (currentTask === currentTime) {
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+            $(this).addClass("present");
         }
         else {
-            $("#text").removeClass("past");
-            $("#text").removeClass("present");
-            $("#text").addClass("future");
+            $(this).removeClass("past");
+            $(this).removeClass("present");
+            $(this).addClass("future");
 
         }
 
@@ -46,14 +51,15 @@ function timer() {
 }
 //add entered text into local storage with the save button. 
 
-$('saveBtn').click(function () {
-    var input = $(this).child('.description').val();
-    var time = $(this).parent().attr('id');
-    localStorage.setItem(input, time)
-});
+
+let loadItems = function() {
+    for(let i = 9; i < 17; i++){
+        $("#"+i + ".discription").val(localStorage.getItem(i));
+    }
+}
 
 timer();
-
+loadItems();
 
 
 
