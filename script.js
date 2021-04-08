5613a187dc48afe42ec0b5155e80e01f14f1ac9e
Albert Rodriguez <alb.rodriguez7@gmail.com>
2021-04-08 11:55:52 -0700

    working on timer

--- script.js
+++ script.js
@@ -2,49 +2,61 @@
 let day = moment().format("dddd, MMMM Do YYYY");
 $("#currentDay").html(day);
 
-auditTask(tasks);
 
-        var tasks = {
-            "9":[],
-            "10":[],
-            "11":[],
-            "12":[],
-            "13":[],
-            "14":[],
-            "15":[],
-            "16":[],
-            "17":[]
+// auditTask(tasks);
+
+// var tasks = {
+//     "9":[],
+//     "10":[],
+//     "11":[],
+//     "12":[],
+//     "13":[],
+//     "14":[],
+//     "15":[],
+//     "16":[],
+//     "17":[]
+// }
+
+//indicate whether the the time blocks are past, present, future. If/else statment might help, use military time.
+function timer() {
+
+    var currentTime = moment().hour()
+
+    $('.time-block').each(function () {
+        var currentTask = parseInt($("#text").attr('id'));
+
+        if ((currentTask) < currentTime) {
+            $("#text").removeClass("future");
+            $("#text").removeClass("present");
+            $("#text").addClass("past");
+        }
+        else if ((currentTask) === currentTime) {
+            $("#text").removeClass("future");
+            $("#text").removeClass("past");
+            $("#text").addClass("present");
+        }
+        else {
+            $("#text").removeClass("past");
+            $("#text").removeClass("present");
+            $("#text").addClass("future");
+
         }
 
-    //indicate whether the the time blocks are past, present, future. If/else statment might help, use military time.
-        
-            var currentHour = moment().hour() 
-        
-            if ((currentTask) > currentTime) {
-                $("#text").addClass("past");
-                $("#text").removeClass("future");
-                $("#text").removeClass("present");
-            }
-            else if ((currentTask) < currentTime) {
-                $("#text").removeClass("future");
-                $("#text").removeClass("past");
-                $("#text").addClass("present");
-            }
-            else ((currentTask) === currentTime); {
-                $("#text").removeClass("past");
-                $("#text").removeClass("present");
-                $("#text").addClass("future");
-                
-            }
-        
-    
-            //add entered text into local storage with the save button. 
-            
-    
-// });
-    
+    })
+}
+//add entered text into local storage with the save button. 
 
-    
+$('saveBtn').click(function () {
+    var input = $(this).child('.description').val();
+    var time = $(this).parent().attr('id');
+    localStorage.setItem(input, time)
+});
+
+timer();
+
+
+
+
 
 
 
